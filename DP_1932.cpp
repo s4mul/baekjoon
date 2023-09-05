9c773f110c12d14c5109656c751daa3dcbbcedd3
Sunwoo Kim <79845005+s4mul@users.noreply.github.com>
2023-09-05 13:59:26 +0900

    해결 DP_1932.cpp
    굳이 dp배열에 얽매이지 말자.
    dp의 강력함은 발상과 그에 따른 효율성의 증가다.
    형식에 얽매이는 것은 그 장점을 깎아먹는다.
    
    top-down과 bottom-up의 차이를 인지하고 햇갈리지 말자.
    하나를 딱 정하고 구현해야 막히지 않는다.
    해결이 길었던 이유는 top-down과 bottom-up의 개념이 혼용되어서였다.

--- DP_1932.cpp
+++ DP_1932.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int ar[500][500];
 int dp[500][500];
 
-void tri(int height);
+int tri(int height);
 int main()
 {
     int n;
@@ -18,24 +18,25 @@ int main()
     scanf("%d", &n);
     
     for(int i =0; i < n; i++){
-        for(int j = 0; k <= i; j++){
+        for(int j = 0; j <= i; j++){
             scanf("%d", &ar[i][j]);
         }
     }
     
-    dp[0][0] = ar[0][0];
+
     
-    tri(1);
+    for(int i = 1; i < n; i++)
+        tri(n - i);
     
+    
+    printf("%d",ar[0][0]);
     return 0;
 }
 
-void tri(int height){
-    for(int i =0 ; i <= height; i++){
-        
-        if(dp[height][i] == -1){
-            if(i == height)
-            dp[height][i] = ar[height][i] + (dp[height - 1][i] > dp[height - 1][i];
-        }
+int tri(int height){//결국 탑다운이네?
+    for(int i =0 ; i <= height; i++){//가로에 있는 모든 원소에 대해
+                ar[height - 1][i] += (ar[height][i] > ar[height][i + 1] ? ar[height][i] : ar[height][i + 1]);
     }
+    return 0;
 }
+
