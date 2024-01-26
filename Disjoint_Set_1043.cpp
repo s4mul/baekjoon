89dbf61029afaee5a23c1cd14e9c00ba77bbf973
Sunwoo Kim <79845005+s4mul@users.noreply.github.com>
2024-01-26 21:25:59 +0900

    find idea
    find idea of problem and have made basic code

--- Disjoint_Set_1043.cpp
+++ Disjoint_Set_1043.cpp
@@ -1,45 +1,39 @@
+
+#include <iostream>
+
+using namespace std;
+
+typedef struct person{
+    int ID;
+    person* next;
+}person;
+
+void find();
+void margeGroup();
+
 int main()
 {
-    int truth_person[51] = {0}, party[50][51];
-    int N, M, able_party, truth_person_amount;
-    int isFalseParty;
+    person people[50];
+    for(int i; i < 50; i++){
+        people[i].next = people[i];
+        people[i].ID = i;
+    }//make all head 
     
-    scanf("%d %d", &N, &M);
-    scanf("%d", &truth_person_amount);
-    for(int i = 0; i < truth_person_amount; i++){
-        scanf("%d", truth_person);//0번째 사람을 tmp값으로 사용
-        truth_person[truth_person[0]] = 1;
-    } //사실을 알고있는 인간과 그렇지 않은 인간으로만 구분하면 됨.
+    int N, M, truthPeople, privID, ID, priorityID;
+    int party[50][50];
     
-  
-    for(int i = 0; i < M; i++){//사실을 들어서 알고있을 사람을 표시
-        scanf("%d", &party[i][0]);
-        isFalseParty = 0;
-        
-        for(int j = 1; j <= party[i][0]; j++){
-            scanf("%d", &party[i][j]);
-            if(truth_person[party[i][j]]) isFalseParty = 1;//party[i][j] = tmp
-        }
-        
-        if(isFalseParty){
-            for(int j = 1; j <= party[i][0]; j++){
-                truth_person[party[i][j]] = 1;
-            }
-        }
-        
-    
+    scanf("%d %d",&N, &M);
+    scanf("%d", &truthPeople);
+    for(int i = 0; i < truthPeople; i++){
+        scanf("%d", &party[0][i])
     }
+    for(int i = 0; i < truthPeople - 1; i++){
+        margeGroup(people + party[0][0], people + party[0][i + 1]);
+    }//merge all truth person
     
-    able_party = 0;
+    priorityID = people[party[0][0]].ID;//head
     for(int i = 0; i < M; i++){
-        isFalseParty = 0;
-        for(int j = 1; j <= party[i][0]; j++){
-            if(truth_person[party[i][j]]) isFalseParty = 1;
-        }
-        able_party += isFalseParty;
+        
     }
-    
-    printf("%d", M - able_party);
-
     return 0;
 }
