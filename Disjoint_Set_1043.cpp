0f6939e5949c524904fb5ef0f149e18bb1091ab5
Sunwoo Kim <79845005+s4mul@users.noreply.github.com>
2024-02-20 19:05:06 +0900

    해결 Disjoint_Set_1043.cpp
    일관성, 모듈화, 주석의 중요성이 들어나는 문제였다.
    풀리지 않던 이유는 파티에 참가한 인원을 끝까지 세주지 않았고 (<= 대신 <을 사용하였다.) 계속 head노드에 대해서 비교해주다 가중치를 주는 부분에서 head노드가 아니라 현재 찾고있는 노드에서 비교하였다. 좀더 알아보기 쉬운, 모듈화되고 일관된 코드 스타일이 필요하다.

--- Disjoint_Set_1043.cpp
+++ Disjoint_Set_1043.cpp
@@ -12,19 +12,19 @@ void margeGroup(person* attendantMaster, person* attendantSlave, int priorityID)
 
 int main()
 {
-    person people[51];
+    person people[100];
     for(int i = 0; i < 51; i++){
         people[i].next = &people[i];
         people[i].ID = i;
     }//make all head 
     
     int N, M, truthPeople, privID, ID, priorityID, partyPeople, cnt;
-    int party[51][51];
+    int party[100][100];
     
     scanf("%d %d",&N, &M);
     scanf("%d", &truthPeople);
     
-    for(int i = 1; i < truthPeople; i++){
+    for(int i = 1; i <= truthPeople; i++){
         scanf("%d", &party[0][i]);
     }
     priorityID = people[party[0][1]].ID;//head -> 사실을 알고 있는 집단을 최고우위로 만들어줌
@@ -42,27 +42,31 @@ int main()
             margeGroup(people + party[i][j],people + party[i][j + 1], priorityID);
         }
     }//해당 과정이 모두 끝나면 그룹화가 끝남
-    
-    cnt = 0;
-    for(int i = 1; i < M; i++){
-        for(int j = 1; j < party[i][j]; j++){
+    cnt = M;
+    for(int i = 1; i <= M; i++){
+        for(int j = 1; j <= party[i][j]; j++){
             if(find(&people[party[i][j]])->ID == priorityID){
+                cnt--;
                 break;
             }
         }
-        cnt++;
+        
     }
     printf("%d", cnt);
     return 0;
 }
 person* find(person* attendant){
+   
     if(attendant->ID == attendant->next->ID){
         return attendant;
     }
+    
     return find(attendant->next);
 }
 void margeGroup(person* attendantMaster, person* attendantSlave, int priorityID){
-    if(attendantSlave->ID == priorityID){
+    if(find(attendantMaster) == find(attendantSlave)) return;
+    
+    if(find(attendantSlave)->ID == priorityID){
         find(attendantMaster)->next = find(attendantSlave);
     }
     else{
