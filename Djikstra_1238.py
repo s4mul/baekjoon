11ba6eb73a6773750a38af758df51068917c87f9
Sunwoo Kim <79845005+s4mul@users.noreply.github.com>
2024-05-17 15:53:25 +0900

    solve Djikstra_1238.py
    in round trip, forword : Djikstra backword: Djikstra with reversed edge
    
    so problem can solve with twice of Djikstra.

--- Djikstra_1238.py
+++ Djikstra_1238.py
@@ -11,9 +11,10 @@ import heapq as h
 class graph:
     def __init__(self, V):
         self.node = [[] for _ in range(V)]
-    
+        self.revnode = [[] for _ in range(V)]
     def addEdge(self, start, end, weight): #V = 0, 1, 2, 3
         self.node[start].append([end,weight])
+        self.revnode[end].append([start,weight])
 #class graph
     
 
@@ -28,22 +29,43 @@ def build_tree(V, E) -> graph:
 
 def Dijkstra(street, X) -> list:
     queue = []
-    h.heappush(queue, (1,X - 1))
+    h.heappush(queue,(1,X - 1))
 
     visited = [0 for _ in range(len(street.node))]
     visited[X - 1] = 1
-    while len(queue) > idx:
+    while queue:
         cor = h.heappop(queue)
-        
+
         for ii in street.node[cor[1]]:
-            if visited == 0 or visited[cor]:
-            visited[ii[0]] = visited[cor] + ii[1]
-            queue.append(ii[0])
+            if visited[ii[0]] == 0 or visited[ii[0]] > visited[cor[1]] + ii[1]:
+                visited[ii[0]] = visited[cor[1]] + ii[1]
+                queue.append((ii[1],ii[0]))
+    
+    return visited
+
+def revDijkstra(street, X) -> list:
+    queue = []
+    h.heappush(queue,(1,X - 1))
+
+    visited = [0 for _ in range(len(street.revnode))]
+    visited[X - 1] = 1
+    while queue:
+        cor = h.heappop(queue)
+
+        for ii in street.revnode[cor[1]]:
+            if visited[ii[0]] == 0 or visited[ii[0]] > visited[cor[1]] + ii[1]:
+                visited[ii[0]] = visited[cor[1]] + ii[1]
+                queue.append((ii[1],ii[0]))
     
     return visited
     
 N, M , X = map(int,sys.stdin.readline().rstrip().split())
 street = build_tree(N,M)
-print(street.node)
-res = BFS(street, X)
-print(res)
+
+res = Dijkstra(street, X)
+res2 = revDijkstra(street, X)
+
+for i in range(len(res)):
+    res[i] = res[i] + res2[i] - 2
+
+sys.stdout.write(str(max(res)))
