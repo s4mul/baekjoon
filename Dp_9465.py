1fac0f472d18b1e4a2027090266f9c9708b795a7
Sunwoo Kim <79845005+s4mul@users.noreply.github.com>
2024-05-19 10:23:30 +0900

    Solve Dp_9465.py
    이전항에 포함되는 경우를 명심하자.

--- Dp_9465.py
+++ Dp_9465.py
@@ -3,25 +3,34 @@ import sys
 sticker = []
 T = int(sys.stdin.readline())
 
-
 for i in range(T):
     N = int(sys.stdin.readline())
     sticker = []
     sticker.append(list(map(int,sys.stdin.readline().split())))
     sticker.append(list(map(int,sys.stdin.readline().split())))
+    DP = [[0 for _ in range(N)],[0 for _ in range(N)]]
+    
+    
+    
     if N == 1:
-        sys.stdout.write(str(max(sticker[0][0], sticker[1][0])+"\n"))
-    if N == 2:
-        sys.stdout.write(str(max(sticker[0][0] + sticker[1][1],sticker[1][0] + sticker[0][1])+"\n"))
+        sys.stdout.write(str(max(sticker[0][0], sticker[1][0]))+"\n")
+        continue
+        
+    elif N == 2:
+        sys.stdout.write(str(max(sticker[0][0] + sticker[1][1],sticker[1][0] + sticker[0][1]))+"\n")
+        continue
+    else:
+        DP[0][0] = sticker[0][0]
+        DP[1][0] = sticker[1][0]
+        DP[0][1] = DP[1][0] + sticker[0][1]
+        DP[1][1] = DP[0][0] + sticker[1][1]
         
     Max = 0
     for i in range(2, N):
-        sticker[0][i] = sticker[0][i]+max(sticker[0][i - 2] + sticker[1][i - 1], sticker[1][i - 2])
-        sticker[1][i] = sticker[1][i]+max(sticker[1][i - 2] + sticker[0][i - 1], sticker[0][i - 2])
-        print(sticker[0])
-        print(sticker[1])
-        print()
-        Max = max(sticker[0][i], sticker[1][i], Max)
+        DP[0][i] = max(DP[1][i - 1], DP[1][i - 2]) + sticker[0][i]
+        DP[1][i] = max(DP[0][i - 1], DP[0][i - 2]) + sticker[1][i]
         
-    sys.stdout.write(str(Max)+"\n")
+
+        
+    sys.stdout.write(str(max(DP[0][-1],DP[1][-1]))+"\n")
     
