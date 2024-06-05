daf388914c100fb8d2ab08d77ba937c93327ae6a
Sunwoo Kim <79845005+s4mul@users.noreply.github.com>
2024-06-05 21:32:38 +0900

    Solved BFS_2206.py
    굳이굳이 꾸역꾸역 이차원배열 하나로 할 필요가 없었다.
    부쉈을때의 차원이 하나만 더 필요하므로 2차원배열 두개로 해결할 수 있었다.

--- BFS_2206.py
+++ BFS_2206.py
@@ -1,54 +1,36 @@
 import sys
 from collections import deque
-movement = [(1,0),(-1,0),(0,1),(0,-1)]
-def trev(x, y, bitmap, movement, mode):
-    queue = deque([]) #x, y, broken
-    queue.appendleft((x,y))
-    bitmap[x][y] = 2 * mode # start point = 2, divided with 0,1
-    
-    while queue:
-        x, y= queue.pop()
-        for dx, dy in movement:
-            if 0<= x + dx < N and 0 <= y + dy < M:
-                if bitmap[x+dx][y+dy] == 0:
-                    bitmap[x+dx][y+dy] = bitmap[x][y] + mode
-                    queue.appendleft((x+dx,y+dy))
-    return (x,y)
 
 N, M = map(int, sys.stdin.readline().rstrip().split())
 #arr[N][M]
-bitmap = [list(map(int,sys.stdin.readline().rstrip())) for _ in range(N)]
+bitmap = [[],[]]
+bitmap[0] = [list(map(int,list(sys.stdin.readline().rstrip()))) for _ in range(N)]
+bitmap[1] = [[0 for __ in range(M)] for _ in range(N)]
 
-shortcut = []
 
-if trev(0, 0, bitmap, movement,1) != (N-1, M-1):#cant reach endpoint
-    trev(N-1,M-1,bitmap,movement,-1)#reverse search
-    for x in range(N):
-        for y in range(M):
-            if bitmap[x][y] == 1:
-                flag = [10000000,-10000000]
-                for dx, dy in movement:
-                    if 0<= x + dx < N and 0 <= y + dy < M:
-                        if bitmap[x+dx][y+dy] > 1 and bitmap[x+dx][y+dy] < flag[0]:
-                            flag[0] = bitmap[x+dx][y+dy]
-                        elif bitmap[x+dx][y+dy] < 0 and bitmap[x+dx][y+dy] > flag[1]:
-                            flag[1] = bitmap[x+dx][y+dy]
-                if flag[0] != 10000000 and flag[1] != -10000000:
-                    shortcut.append(flag[0] - flag[1] -1)
-    if len(shortcut) > 0:
-        print(min(shortcut))
-    else:
-        print(-1)
-else:
-    shortcut.append(0)
-    for x in range(N):#can reach endpoint
-        for y in range(M):
-            if bitmap[x][y] == 1:
-                near = []
-                for dx, dy in movement:
-                    if 0<= x + dx < N and 0 <= y + dy < M and (bitmap[x+dx][y+dy] != 0 and bitmap[x+dx][y+dy] != 1):
-                        near.append(bitmap[x+dx][y+dy])
-                if len(near) >= 2:
-                    shortcut.append(max(near) - min(near) - 2)
+movement = [(1,0),(-1,0),(0,1),(0,-1)]
+queue = deque([]) #x, y, broken
+queue.appendleft((0,0,0))
+bitmap[0][0][0] = 2 # start point = 2, divided with 0,1
+idx = 0
+
+while queue:
+    broken, x, y= queue.pop()
+    if x == N-1 and y == M - 1:
+        ans = bitmap[broken][x][y] -1
+        break
     
-    print(bitmap[-1][-1] - max(shortcut)-1)
+    for dx, dy in movement:
+        if 0<= x + dx < N and 0 <= y + dy < M:
+            
+            if broken == 0 and bitmap[broken][x+dx][y+dy] == 1:
+                bitmap[1][x+dx][y+dy] = bitmap[0][x][y] + 1
+                queue.appendleft((1,x+dx,y+dy))
+                
+            elif bitmap[broken][x+dx][y+dy] == 0 and bitmap[0][x+dx][y+dy] == 0:
+                bitmap[broken][x+dx][y+dy] = bitmap[broken][x][y] + 1
+                
+                queue.appendleft((broken,x+dx,y+dy))
+    ans = -1
+
+print(ans)
